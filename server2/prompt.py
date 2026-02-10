0d40c96a617d4661de555cba193d23394cffa086
摆烂Jo <32508140+zhouajiew@users.noreply.github.com>
2026-02-10 18:26:09 +0800

    Update prompt.py

--- server2/prompt.py
+++ server2/prompt.py
@@ -2,6 +2,7 @@ import os
 import re
 from datetime import datetime
 
+from character import *
 from deepseek import *
 from memory import *
 from translation import *
@@ -74,11 +75,18 @@ def system_prompt(who):
 
     if not get_character_prompt:
         if current_assistant[0] == 'Bandit':
-            with open(f"{relative_path}/设定.txt", "r", encoding='utf-8') as file:
-                character_prompt = file.read()
+            if app_environment[0] == 0:
+                with open(f"{relative_path}/设定.txt", "r", encoding='utf-8') as file:
+                    character_prompt = file.read()
+            else:
+                with open(f"{relative_path}/设定2.txt", "r", encoding='utf-8') as file:
+                    character_prompt = file.read()
         else:
             with open(f"{relative_path}/设定({current_assistant[0]}).txt", "r", encoding='utf-8') as file:
                 character_prompt = file.read()
+
+            if enable_likeability[0] == 1:
+                character_prompt = change_character_setting(character_prompt, current_likeability[0], user_name[0])
 
     if character_prompt != "":
         temp_character_prompt = character_prompt
@@ -183,7 +191,7 @@ def get_translation_part2(memories, important_info_content, num, user_content, a
         get_important_info_and_rewrite2(who, translation, current_assistant[0])
 
 # Translation部分prompt(分段获取)
-def get_translation_part(memories, important_info_content, num, user_content, assistant_content, who, assistant, read_index):
+async def get_translation_part(memories, important_info_content, num, user_content, assistant_content, who, assistant, read_index):
     global get_translation_prompt
     global translation_prompt
     global task_finished_status
@@ -241,16 +249,12 @@ def get_translation_part(memories, important_info_content, num, user_content, as
         part2_text += p
 
     prompt.append({"role": "user", "content": part2_text})
-    # 2025/8/20 公益站已无法打开
-    # 从公益站获取回复，按次数收费减少开销，速度也会快一点
     # print("正在等待处理重要信息...\n")
 
     result = {}
 
     try:
-        # result = get_ai_response3_stream("DeepSeek-R1-0528", prompt, who)
-        # result = get_ai_response2_stream(model2[0], prompt, who)
-        result = get_ai_response2_stream("deepseek-chat", prompt, who)
+        result = await get_ai_response("deepseek-chat", prompt, who)
         task_finished_status += 1
 
         with open(f"{relative_path}/p{read_index}_result.txt", "w", encoding='utf-8') as file:
@@ -262,20 +266,22 @@ def get_translation_part(memories, important_info_content, num, user_content, as
 
     translation = ""
     pattern = r'[\({]Translation[:：]([\s\S]*)[\)}]'
-    if re.search(pattern, result["content"]):
-        # 确保Translation中不含有None
-        if 'None' not in re.search(pattern, result["content"]).group():
-            translation = re.search(pattern, result["content"]).group()
-            translation = re.sub(pattern, r'\1', translation)
 
-            # 去掉translation中多余的符号
-            pattern = r'[{}]'
-            translation = re.sub(pattern, r'', translation)
+    if result.get('content'):
+        if re.search(pattern, result["content"]):
+            # 确保Translation中不含有None
+            if 'None' not in re.search(pattern, result["content"]).group():
+                translation = re.search(pattern, result["content"]).group()
+                translation = re.sub(pattern, r'\1', translation)
 
-            # print("获取到的翻译:" + translation)
+                # 去掉translation中多余的符号
+                pattern = r'[{}]'
+                translation = re.sub(pattern, r'', translation)
 
-    if len(translation) > 0:
-        get_important_info_and_rewrite2(who, translation, current_assistant[0])
+                # print("获取到的翻译:" + translation)
+
+        if len(translation) > 0:
+            get_important_info_and_rewrite2(who, translation, current_assistant[0])
 
 # 示例对话提示词
 def example_prompt(who):
@@ -304,6 +310,159 @@ def many_events_needed_prompt(new_message):
 
     return all_ep
 
+# 时间跨度提取提示词:
+def get_time_span_prompt(new_message, memories, num, who):
+    # 今天/昨天/上个星期/上个月/去年/n天前/n个星期前/n个月前/n年前
+    time_span_prompt = []
+    time_span_prompt.append("你正在和某个用户对话。\n")
+    time_span_prompt.append("这位用户给你发送了新消息，你接下来给这位用户的回复中可能需要提及某个具体时间段内的事情。")
+    time_span_prompt.append("请你从'完整的对话内容'以及'用户新的消息'中提取你最可能需要参考的某个具体时间段，只需要输出'时间关键词'中的一个关键词！")
+    time_span_prompt.append("# 时间关键词\n")
+    time_span_prompt.append("今天/昨天/上个星期/上个月/去年/n天前/n个星期前/n个月前/n年前\n\n")
+    time_span_prompt.append("# 注意事项\n")
+    time_span_prompt.append("1. 如果提取出的时间信息包含n，请尝试将n转换为具体的阿拉伯数字！如果无法转换为具体的阿拉伯数字，则不需要转换n！\n")
+    time_span_prompt.append("2. 如果无法提取出任何时间信息，直接输出'无'这个汉字")
+    time_span_prompt.append("# 输出示例\n1.\n")
+    time_span_prompt.append("用户新的消息: Do you still remember what we have done yesterday?\n")
+    time_span_prompt.append("你的输出：昨天\n")
+    time_span_prompt.append("2.\n用户新的消息: I guess he did those things three days ago.\n")
+    time_span_prompt.append("你的输出: 3天前\n")
+    time_span_prompt.append("3.\n用户新的消息: I don't know, maybe it happened a few weeks ago.\n")
+    time_span_prompt.append("你的输出: n个星期前\n")
+    time_span_prompt.append("4.\n用户新的消息: Nice to meet you!\n")
+    time_span_prompt.append("你的输出: 无\n\n")
+
+    time_span_prompt.append("我会在下一次对话中给你提供'完整的对话内容'以及'用户新的回复'，如果你完全理解了上述内容，请回复'已理解'。")
+
+    time_span_prompt_str = ""
+
+    for tsp in time_span_prompt:
+        time_span_prompt_str += tsp
+
+    prompt = [{"role": "system", "content": time_span_prompt_str}]
+    prompt.append({"role": "assistant", "content": '已理解'})
+
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    part2 = [f"当前对话的时间为{timestamp}\n"]
+    part2_text = ""
+    part2.append("# 完整的对话内容\n")
+
+    history_content = ""
+
+    if len(memories) > num:
+        temp_memories = memories[-num:]
+        for idx, m in enumerate(temp_memories):
+            history_content += f"对话{idx + 1}:\n用户发送该消息的时间为:\n{m['timestamp']}\n用户的消息: \n{m['user_content']}\n你的回复: \n{m['assistant_content']}\n\n"
+    else:
+        for idx, m in enumerate(memories):
+            history_content += f"对话{idx + 1}:\n用户发送该消息的时间为:\n{m['timestamp']}\n用户的消息: \n{m['user_content']}\n你的回复: \n{m['assistant_content']}\n\n"
+
+    part2.append(history_content)
+    part2.append("用户新的消息:\n")
+    part2.append(f"{new_message}\n")
+
+    for p in part2:
+        part2_text += p
+
+    prompt.append({"role": "user", "content": part2_text})
+
+    try:
+        # result = get_ai_response3_stream("DeepSeek-R1-0528", prompt, who)
+        # result = get_ai_response2_stream(model2[0], prompt, who)
+        result = get_ai_response2_stream("deepseek-chat", prompt, who)
+
+        with open(f"{relative_path}/time_span.txt", "w", encoding='utf-8') as file:
+            full_content = f'completion_tokens:{result["completion_tokens"]}\nprompt_cache_hit_tokens:{result["prompt_cache_hit_tokens"]}\nprompt_cache_miss_tokens:{result["prompt_cache_miss_tokens"]}\n\n本次请求消费{result["cost"]}元\n\n{result['content']}'
+            file.write(full_content)
+
+        return result["content"]
+    except Exception as e:
+        print(f"时间跨度提取失败!{e}")
+
+        return '无'
+
+# 修改好感度
+async def change_likeability(new_message, your_reply, likeability, memories, num, who):
+    likeability_prompt = []
+    likeability_prompt.append("你正在和某个用户对话。\n")
+    likeability_prompt.append("这位用户给你发送了新消息，你也作出了相应的回复。\n")
+    likeability_prompt.append("请参考'完整的对话内容'、'用户新的消息'以及'你的回复'，结合'好感度数值及其说明'输出你对这位用户的最新的好感度数值。\n\n")
+    likeability_prompt.append("# 好感度数值及其说明\n")
+    likeability_prompt.append("0~5 你对这位用户恨之入骨，恨不得生啖其肉/6~10 这位用户就像你的杀父仇人，手刃仇敌方快哉/11~20 这位用户是你的眼中钉，肉中刺/")
+    likeability_prompt.append("21~25 你十分讨厌这位用户/26~30 你很讨厌这位用户/31~35 你有些讨厌这位用户/")
+    likeability_prompt.append("36~40 你有些看不惯这位用户，但并不会说出来/41~50 你对这位用户观感不是太好，但还是能普通对待亦或者真的只是完全普通的路人程度的观感/")
+    likeability_prompt.append("51~60 你对这位用户观感还不错/61~70 这位用户是你的普通朋友/")
+    likeability_prompt.append("71~80 这位用户是和你关系比较好的朋友/81~85 这位用户是你非常要好的朋友/86~89 这位用户是你最好的朋友，是你的大亲友/90~92 你和这位用户的关系是亲友以上恋爱未满，换成友情就是知音/")
+    likeability_prompt.append("93~94 你对这位用户十分有好感，快要恋爱的程度，或者是十分要好的知音/95~96 你和这位用户已经恋爱了，亦或者说是生死兄弟/")
+    likeability_prompt.append("97~98 你和这位用户坠入爱河不可自拔，伯牙子期那种程度的友情/99~100 你对这位用户的爱就像人类对于爱情的最高程度的爱，亦或者到了生死不离程度的友情")
+    likeability_prompt.append("# 注意事项\n")
+    likeability_prompt.append("1. 你只需要输出一个具体的整数，即好感度数值！\n")
+    likeability_prompt.append("2. 好感度数值的最低值为0，最高值为100\n，好感度数值越高，说明你和这位用户的关系越好")
+    likeability_prompt.append("3. 在本次对话结束后，你可以增加或减少你对这位用户的好感度数值，也可以保持该数值不变\n")
+    likeability_prompt.append("4. 最终输出的好感度数值与'在本次对话之前，你对这位用户的好感度数值'的绝对值差值必须小于等于5！\n\n")
+
+    likeability_prompt.append(
+        "我会在下一次对话中给你提供'完整的对话内容'、'用户新的回复'以及'你的回复'，如果你完全理解了上述内容，请回复'已理解'。")
+
+    likeability_prompt_str = ""
+
+    for lp in likeability_prompt:
+        likeability_prompt_str += lp
+
+    prompt = [{"role": "system", "content": likeability_prompt_str}]
+    prompt.append({"role": "assistant", "content": '已理解'})
+
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    part2 = [f"当前对话的时间为{timestamp}\n"]
+    part2_text = ""
+    part2.append("# 完整的对话内容\n")
+
+    history_content = ""
+
+    if len(memories) > num:
+        temp_memories = memories[-num:]
+        for idx, m in enumerate(temp_memories):
+            history_content += f"对话{idx + 1}:\n用户发送该消息的时间为:\n{m['timestamp']}\n用户的消息: \n{m['user_content']}\n你的回复: \n{m['assistant_content']}\n\n"
+    else:
+        for idx, m in enumerate(memories):
+            history_content += f"对话{idx + 1}:\n用户发送该消息的时间为:\n{m['timestamp']}\n用户的消息: \n{m['user_content']}\n你的回复: \n{m['assistant_content']}\n\n"
+
+    part2.append(history_content)
+    part2.append(f"在本次对话之前，你对这位用户的好感度数值:{likeability}\n")
+    part2.append("用户新的消息:\n")
+    part2.append(f"{new_message}\n")
+    part2.append("你的回复:\n")
+    part2.append(f"{your_reply}\n")
+
+    for p in part2:
+        part2_text += p
+
+    prompt.append({"role": "user", "content": part2_text})
+
+    try:
+        result = await get_ai_response("deepseek-chat", prompt, who)
+
+        pattern = r'\d+'
+        if re.search(pattern, result['content']):
+            temp_likeability = re.search(pattern, result['content']).group()
+            current_likeability[0] = int(temp_likeability)
+
+        with open(f"{relative_path}/private/{who}/likeability({current_assistant[0]}).txt", "w", encoding='utf-8') as file:
+            full_content = result['content']
+            file.write(full_content)
+
+        with open(f"{relative_path}/likeability.txt", "w", encoding='utf-8') as file:
+            full_content = f'completion_tokens:{result["completion_tokens"]}\nprompt_cache_hit_tokens:{result["prompt_cache_hit_tokens"]}\nprompt_cache_miss_tokens:{result["prompt_cache_miss_tokens"]}\n\n本次请求消费{result["cost"]}元\n\n{result['content']}'
+            file.write(full_content)
+
+        return result["content"]
+    except Exception as e:
+        print(f"修改好感度失败!{e}")
+
+        return '无'
+
 # 情绪识别
 def emotion_recognition(memories, num, user_content, assistant_content, who):
     emotion_prompt = []
@@ -325,7 +484,7 @@ def emotion_recognition(memories, num, user_content, assistant_content, who):
     emotion_prompt.append("horror/interest/joy/nostalgia/relief/")
     emotion_prompt.append("romance/sadness/satisfaction/sexual/")
     emotion_prompt.append("surprise\n\n")
-    emotion_prompt.append("我会在下一次对话中给你提供'完整的对话内容'以及'用户新的回复'，如果你完全理解了上述内容，请回复'已理解'。")
+    emotion_prompt.append("我会在下一次对话中给你提供'完整的对话内容'以及'用户新的消息'，如果你完全理解了上述内容，请回复'已理解'。")
 
     emotion_prompt_str = ""
 

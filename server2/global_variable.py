4e02a14abad1326f213809c8552754bc37b26bdd
摆烂Jo <32508140+zhouajiew@users.noreply.github.com>
2026-02-10 18:24:42 +0800

    Update global_variable.py

--- server2/global_variable.py
+++ server2/global_variable.py
@@ -5,3 +5,11 @@ api_key = [""]
 model2 = ["deeppeek-chat"]
 api_key2 = [""]
 api_key3 = [""]
+
+# 是否开启好感度
+enable_likeability = [0]
+# 当前assistant对当前user的好感度
+current_likeability = [50]
+
+# 显示对话时间
+show_dialog_time = [0]
